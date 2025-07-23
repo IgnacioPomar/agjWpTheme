bc088a860b5f895bad4d7f1d88697fdd1bf09f92
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-07-23 17:35:36 +0200

    Formateado el footer

--- footer.php
+++ footer.php
@@ -3,18 +3,39 @@
 	
 
 	<!-- TODO: get email and usr of "aviso Legal" from its page ID -->
+	
 	<?php
+	$separator = '';
+	echo '<p class="copyright">';
 	$legalPageId = get_option ('agj_legal_page');
-	$legalUrl = get_permalink ($legalPageId);
-	$infoEmail = get_option ('agj_email');
+	if ($legalPageId)
+	{
+		$url = get_permalink ($legalPageId);
+		echo '<a href="' . $url . '">Aviso</a>';
+
+		$separator = '<span class="separator">|</span>';
+	}
+	$privacyPageId = get_option ('agj_privacy_page');
+	if ($privacyPageId)
+	{
+		$url = get_permalink ($privacyPageId);
+		echo $separator . '<a href="' . $url . '">Po&iacute;tica de privacidad</a>';
+
+		$separator = '<span class="separator">|</span>';
+	}
+	$cookiePageId = get_option ('agj_cookie_page');
+	if ($cookiePageId)
+	{
+		$url = get_permalink ($cookiePageId);
+		echo $separator . '<a href="' . $url . '">Po&iacute;tica de cookies</a>';
+	}
+
+	echo '</p>';
 	?>
 	
-		<p class="email"><a href="<?=$infoEmail?>"><?=$infoEmail?></a></p>
-		<p class="copyright"><a href="<?=$legalUrl?>">AVISO LEGAL</a></p>
-		<p>&copy; <?=date ("Y")?> Alternative Experience&reg;. Todos los derechos reservados.</p>
+	<p>&copy; <?=date ("Y")?> Alternative Experience&reg;. Todos los derechos reservados.</p>
 	</div>
 	<?php
-
 	wp_footer ();
 	?>
 </body>
